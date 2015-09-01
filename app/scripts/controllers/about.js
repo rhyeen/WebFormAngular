1742c7551eeb56accb9623c064e39c539eec69ab
Ryan Saunders <contact.ryan.saunders@gmail.com>
2015-09-01 16:27:04 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ddAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ddAngularApp
+ */
+angular.module('ddAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ddAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ddAngularApp
+ */
+angular.module('ddAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
